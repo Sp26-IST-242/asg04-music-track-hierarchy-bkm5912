85f54ff3cc3cf9e3c71967e988319bff74bddd90
bkm5912 <sam@lehighradio.com>
2026-03-21 14:54:22 -0400

    Add songs to main.py first

--- main.py
+++ main.py
@@ -18,3 +18,18 @@ After sorting:
 (Sarah Koenig, Journalism) Serial active = False,  debut year: 2014, duration: 01:30:00 is explicit: False
 (Kendrick Lamar, Hip-Hop) DAMN. active = True,  debut year: 2017, duration: 03:40
 """
+from artist import Artist
+from album import Album
+from song import Song
+from podcast import Podcast
+from playlist import Playlist
+
+if __name__ == "__main__":
+    # make the songs first
+    artist1 = Artist("Kendrick Lamar", "Hip-Hop")
+    album1 = Album("DAMN.", True, [2017, 2018])
+    song1 = Song(artist1, album1, 220)
+
+    artist2 = Artist("Alanis Morissette", "Alternative")
+    album2 = Album("Jagged Little Pill", False, [1995, 1996])
+    song2 = Song(artist2, album2, 245)
